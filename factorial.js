978d9712f5f46cdbdc5e98f6af5ec42d3efb8dfd
Minnia <jasminestockholm@gmail.com>
2020-01-13 06:30:20 +0100

    add code for factorial.js

--- factorial.js
+++ factorial.js
@@ -1,1 +1,10 @@
-//Rewrite code
+const factorial = num => {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  for (let i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+};
+console.log(factorial(4)); //returns 24
