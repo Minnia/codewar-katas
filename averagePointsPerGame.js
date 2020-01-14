370f1df52975b684ec964f21832e8f6a384e3f9f
Minnia <jasminestockholm@gmail.com>
2020-01-14 22:09:58 +0100

    add code for file averagePointsPerGame

--- averagePointsPerGame.js
+++ averagePointsPerGame.js
@@ -1,1 +1,8 @@
 //Rewrite code
+const pointsPer48 = (ppg, mpg) => {
+  const exactScore = (ppg / mpg) * 48;
+  const extrapolatedScore = Math.round(10 * exactScore) / 10;
+  return extrapolatedScore;
+};
+
+console.log(pointsPer48(12, 20));
