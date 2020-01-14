dfe2c9d3c7ea79fa16009f2730c03d800d4cc9a2
Minnia <jasminestockholm@gmail.com>
2020-01-14 22:23:30 +0100

    add code to createPhoneNumber file

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -1,1 +1,20 @@
 //Rewrite code
+const createPhoneNumber = numbers => {
+  const formatPhoneNumber = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (i === 0) {
+      formatPhoneNumber.push("(" + numbers[i]);
+    } else if (i === 2) {
+      formatPhoneNumber.push(numbers[i] + ")");
+    } else if (i === 3) {
+      formatPhoneNumber.push(" " + numbers[i]);
+    } else if (i === 5) {
+      formatPhoneNumber.push(numbers[i] + "-");
+    } else {
+      formatPhoneNumber.push(numbers[i]);
+    }
+  }
+  return formatPhoneNumber.join("");
+};
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // (1,2,3) 456-7890
