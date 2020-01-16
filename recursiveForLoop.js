6fa3bd9227630cbc2c45b99d75dc27c85f007402
Minnia <jasminestockholm@gmail.com>
2020-01-17 00:22:34 +0100

    add functionality to recursiveForLoop.js

--- recursiveForLoop.js
+++ recursiveForLoop.js
@@ -1,7 +1,11 @@
+//Simple recursive loop
+//Counts down from 100 to 1
+//The challenge was to let i start at 0 while counting down from 100
+
 let count = 0;
 let number = 100;
 
-for (let i = 0; i <= number; i++) {
+for (let i = 0; i < 100; i++) {
   number--;
   count++;
   console.log(i, number, count);
