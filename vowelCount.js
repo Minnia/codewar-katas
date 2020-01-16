49dbf02c5c01ffbba453925917fe5622069609e9
Minnia <jasminestockholm@gmail.com>
2020-01-17 00:17:37 +0100

    att functionality to vowelCount.js

--- vowelCount.js
+++ vowelCount.js
@@ -1,1 +1,9 @@
 //Rewrite code
+
+function getCount(str) {
+  const arrayOfVowels = ["a", "e", "i", "o", "u"];
+  const matchingVowels = [...str].filter(char => arrayOfVowels.includes(char));
+  return matchingVowels.length;
+}
+
+console.log(getCount("hello")); //2
